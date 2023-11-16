5fc5be9dcbd0ff19e08d4a59ce7a457f4831ee1c
Palak <Palakgoyal0304@gmail.com>
2023-11-16 18:57:06 +0530

    refactor function

--- index.js
+++ index.js
@@ -4,28 +4,12 @@ let guestScore = 0;
 let homeScoreEl = document.getElementById("home-score")
 let guestScoreEl = document.getElementById("guest-score")
 
-function add1() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore;
-}
-function add2() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore;
-}
-function add3() {
-    homeScore += 3
+function add(n) {
+    homeScore += n
     homeScoreEl.textContent = homeScore;
 }
 
-function addG1() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore;
-}
-function addG2() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore;
-}
-function addG3() {
-    guestScore += 3
+function addG(n) {
+    guestScore += n
     guestScoreEl.textContent = guestScore;
 }
